9c62541373ac551004e2a624253519b5d865f1d1
Rohan Krishnan <rohankrishnan@live.com>
2023-08-02 22:31:49 -0700

    1word2vec

--- codenames.py
+++ codenames.py
@@ -1,14 +1,15 @@
 from playgroundrl.client import *
 from playgroundrl.actions import *
-from word2vec import *
+import word2vec
 
 BOARD_SIZE = 25
+
 
 class TestCodenames(PlaygroundClient):
     def __init__(self):
         super().__init__(
             GameType.CODENAMES,
-            model_name="rohan-codenames",
+            model_name="1word2vec",
             auth={
                 "email": "rohankrishnan00@gmail.com",
                 "api_key": "N038MxaxBpktIgGP40Yk3u1rmm2FP7x0THpPufnsHsM",
@@ -21,26 +22,24 @@ class TestCodenames(PlaygroundClient):
             return None
 
         if state.role == "GIVER":
-            word, count = clue(state)
-            return CodenamesSpymasterAction(
-                word = word, 
-                count = count 
-            )
+            word, count = word2vec.clue(state)
+            return CodenamesSpymasterAction(word=word, count=count)
         elif state.role == "GUESSER":
             return CodenamesGuesserAction(
-                guesses= guess(state),
+                guesses=word2vec.guess(state),
             )
 
     def gameover_callback(self):
         pass
 
+
 if __name__ == "__main__":
     # args = parse_arguments("codenames")
     t = TestCodenames()
     t.run(
-        # pool=Pool(),
-        # num_games=args.num_games,
-        # self_training=True,
+        # pool=Pool.OPEN,
+        # num_games=10,
+        self_training=True,
         # maximum_messages=500000,
         # used to set up 2-player game (rather than default 4)
         # game_parameters={"num_players": 2},
