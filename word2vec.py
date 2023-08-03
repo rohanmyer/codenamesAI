7e5d132b76142cd751ecd493d3ed0de6a4f6f5a7
Rohan Krishnan <rohankrishnan@live.com>
2023-08-02 22:30:45 -0700

    1word2vec

--- word2vec.py
+++ word2vec.py
@@ -48,23 +48,6 @@ def guess(state) -> list:
     return [words_scores[i][0] for i in range(state.count)]
 
 
-def find_optimal_clusters(data, max_clusters):
-    distortions = []
-
-    for num_clusters in range(1, max_clusters + 1):
-        kmeans = KMeans(n_clusters=num_clusters, random_state=42)
-        kmeans.fit(data)
-        distortions.append(
-            kmeans.inertia_
-        )  # Sum of squared distances to the nearest cluster center
-
-    # Select the optimal number of clusters based on the Elbow point
-    # (where the distortion value stops decreasing significantly)
-    elbow_point = np.argmin(np.gradient(distortions))
-
-    return elbow_point + 1
-
-
 def clue(state) -> tuple:
     words = state.words
     guessed = state.guessed
@@ -112,16 +95,3 @@ def clue(state) -> tuple:
             clue = w
             sim = close[0][1]
     return clue, min(count, 3)
-    # word_vects = []
-    # for i, word in enumerate(words):
-    #     if guessed[i] == "UNKNOWN" and actuals[i] == state.color:
-    #         word_vects.append((word, model[word]))
-
-    # num_clusters = find_optimal_clusters([x[1] for x in word_vects], len(word_vects)//2)
-    # kmeans = KMeans(n_clusters=num_clusters, random_state=42)
-    # kmeans_model = kmeans.fit([x[1] for x in word_vects])
-    # clue_vect = kmeans_model.cluster_centers_[0]
-
-    # clue = model.similar_by_vector(clue_vect, topn=1)[0][0]
-    # clue_words = [x[0] for x in word_vects if kmeans_model.predict([x[1]])[0] == 0]
-    # return clue, len(clue_words)
